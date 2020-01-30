4e94ff65dc6cabc4ee74e36b1572717e7c240e96
Intotheskyy <max.gtn@yandex.ru>
2020-01-30 13:06:04 +0300

    v3

--- features/step_definitions/feedback_screen_steps.rb
+++ features/step_definitions/feedback_screen_steps.rb
@@ -10,6 +10,13 @@ sleep 1
 end
 
 Then(/^I see Feedback screen$/) do
+  find_element(:id, 'text_content')
   find_element(:id, 'checkbox_system_logs')
   raise 'Checkbox is not checked' if find_element(:id, 'checkbox_system_logs').attribute('checked') == 'false'
+  find_element(:id, 'checkbox_device_info')
+  raise 'Checkbox is not checked' if find_element(:id, 'checkbox_device_info').attribute('checked') == 'false'
+  find_element(:id, 'checkbox_attach_screenshot')
+  raise 'Checkbox is not checked' if find_element(:id, 'checkbox_attach_screenshot').attribute('checked') == 'false'
+  find_element(:id, 'screenshot_image')
+  find_element(:id, 'send_feedback')
 end
